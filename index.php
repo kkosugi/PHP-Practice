678d21e62a0588f0ade7b81f23c4c512b885158d
kohei kosugi <klam195310@gmail.com>
2018-01-29 11:57:09 +0900

    Edit Function pager and some view page

--- index.php
+++ index.php
@@ -16,42 +16,43 @@
 
       <hr>
 
-      <!-- <script type="text/javascript">
-      alert("javascript-test");
-      </script> -->
-      <form class="form-horizontal" method="post" action="upload.php">
-      	<div class="form-group">
-          <label class="col-sm-2 control-label" for="name">name</label>
-          <div class="col-sm-10">
-            <input type="name" class="form-control" id="InputEmail" placeholder="name">
-      		<label class="col-sm-2 control-label" for="InputEmail">mailadress</label>
-      		<div class="col-sm-10">
-      			<input type="email" class="form-control" id="InputEmail" placeholder="maildress">
-      		</div>
-      	</div>
-      	<div class="form-group">
-      		<label class="col-sm-2 control-label" for="InputPassword">パスワード</label>
-      		<div class="col-sm-10">
-      			<input type="password" class="form-control" id="InputPassword" placeholder="パスワード">
-      		</div>
-        </div>
-        <p><button class ="btn" type="submit">送信</button></p>
-      </form>
+        <form class="form-horizontal" method="post" action="upload.php">
+        	<div class="form-group">
 
-      <form class ="form-horizontal" method="post" action="upload.php">
-        <h2  "name">name</h2>
-        <input class="btn"type="text" name="n" placeholder="name" required/>
-        <h2 class = "mail">mail address</h2>
-        <input class="btn-default" name="m" placeholder="mailadress" />
-        <h2 class = "comment">comment</h2>
-        <textarea name="c" placeholder="comment"></textarea>
+            <label class="col-sm-1 control-label" for="name">name</label>
+            <div class="col-sm-11">
+              <input type="name" class="form-control" id="inputname" type="text" name="n" placeholder="name" required>
+              <br></br>
+            </div>
+
+        		<label class="col-sm-1 control-label" for="InputEmail">mailadress</label>
+        		<div class="col-sm-11">
+        			<input type="email" class="form-control" id="InputEmail" name="m" placeholder="maildress">
+              <br></br>
+        		</div>
+
+        		<label class="col-sm-1 control-label" for="InputPassword">comment</label>
+        		<div class="col-sm-11">
+        			<input type="comment" class="form-control" id="InputPassword" name="c" placeholder="comment">
+        		</div>
+            <br></br>
+
+            <div><button class ="btn" type="submit" >送信</button></div>
+
+          </div>
+
+        </form>
 
       <hr>
 
       <h2>登録データ</h2>
 
-      <ul>
-
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="#">Home</a></li>
+          <li class="breadcrumb-item"><a href="#">music</a></li>
+          <li class="breadcrumb-item active">pop</li>
+        </ol>
+      </div>
 <?php
 require_once "db_setting.php";
 date_default_timezone_set('Asia/Tokyo');
@@ -61,31 +62,125 @@ try {
   $dbh = new PDO('mysql:host='.$host.';dbname='.$database, $user, $pass);
   foreach($dbh->query('SELECT * from '.$table) as $row) {
       // print_r($row);
-      echo "<li class='sentence'>";
-      echo "<span class='id'>".$row["UserID"]."</span> : ";
-      echo "<span class='name'>".$row["name"]."</span>";
-      echo "<span class='mailadress'>".$row["mailadress"]."</span>";
-      echo "<span class='times'>".$row["times"]."</span>";
+
+      echo "<table class='table table-hover'>";
+        echo "<thead>";
+          echo "<tr>";
+          echo "<th scope='col'>nickname</th>";
+          echo "<th scope='col'>".$row["name"]."</th>";
+          echo "<th scope='col'>user_id</th>";
+          echo "<th scope='col'>".$row["UserID"]."</th>";
+          echo "<th scope='col'>time</th>";
+          echo "<th scope='col'>".$row["times"]."</th>";
+          echo "</tr>";
+        echo "</thead>";
+
+      echo "<tbody>";
+        echo "<tr class='table-primary'>";
+          echo "<th scope='row'>mailadress</th>";
+            echo "<td>".$row["mailadress"]."</td>";
+        echo "</tr>";
+
+        echo "<tr class='table-successs'>";
+          echo "<th scope='row'>comment</th>";
+            echo "<td>".$row["comment"]."</td>";
+        echo "</tr>";
+      echo "</table>";
+
       echo "<form method='post' action='delete.php'>";
       echo "<input type='hidden' name='UserID' value='".$row["UserID"]."'>";
-      echo "<button type='submit'>delete</button>";
-      echo "<button type='submit'>edit</button>";
+      echo "<button class ='btn btn-primary btn-lg color: #00b0f0;' type='submit'>delete</button>";
+      echo "<button class ='btn btn-primary btn-lg' id ='ebtn' type='submit'>edit</button>";
       echo "</div>";
 
       echo "</form>";
       echo "</li>";
 
   }
-  $totalPage = 10;
-  if (
-    isset($_GET["page"]) &&
-    $_GET["page"] > 0 &&
-    $_GET["page"] <= $totalPage
-  ) {
-    $page = (int)$_GET["page"];
-  } else {
-    $page = 1;
-  }
+
+  // <table class="table table-hover">
+  //   <thead>
+  //     <tr>
+  //       <th scope="col">Type</th>
+  //       <th scope="col">Column heading</th>
+  //       <th scope="col">Column heading</th>
+  //       <th scope="col">Column heading</th>
+  //     </tr>
+  //   </thead>
+  //   <tbody>
+  //     <tr class="table-active">
+  //       <th scope="row">Active</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr>
+  //       <th scope="row">Default</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-primary">
+  //       <th scope="row">Primary</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-secondary">
+  //       <th scope="row">Secondary</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-success">
+  //       <th scope="row">Success</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-danger">
+  //       <th scope="row">Danger</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-warning">
+  //       <th scope="row">Warning</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-info">
+  //       <th scope="row">Info</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-light">
+  //       <th scope="row">Light</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //     <tr class="table-dark">
+  //       <th scope="row">Dark</th>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //       <td>Column content</td>
+  //     </tr>
+  //   </tbody>
+  // </table>
+
+  // $totalPage = 10;
+  // if (
+  //   isset($_GET["page"]) &&
+  //   $_GET["page"] > 0 &&
+  //   $_GET["page"] <= $totalPage
+  // ) {
+  //   $page = (int)$_GET["page"];
+  // } else {
+  //   $page = 1;
+  // }
 
   $dbh = null;
 } catch (PDOException $e) {
@@ -94,17 +189,15 @@ try {
 }
 
 ?>
-
-<p class = page>
-    <?php if ($page > 1) : ?>
-       <a href="?page=<?php echo ($page - 1); ?>"><</a>
-    <?php endif; ?>
-
-    <?php echo $page; ?>
-
-    <?php if ($page < $totalPage) : ?>
-    <a href="?page=<?php echo ($page + 1); ?>">></a>
-    <?php endif; ?>
-</p>
+<nav>
+  <ul class="pager">
+    <li class="disabled"><a href="">&laquo;</a></li>
+    <li class="active"><a href="">1</a></li>
+    <li><a href="">2</a></li>
+    <li><a href="">3</a></li>
+    <li><a href="">4</a></li>
+    <li><a href="">&raquo;</a></li>
+  </ul>
+</nav>
   </body>
 </html>
